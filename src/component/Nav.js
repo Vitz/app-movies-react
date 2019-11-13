9f4cc74089b7c4da44823eb6731a5f506306d93e
Vitz <viton007@op.pl>
2019-11-13 11:33:40 +0100

    rm logo

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
         <a className="navbar-brand" href="/">
-          <img src="https://png2.cleanpng.com/sh/63c5cba8e6b11ffe4e9608fafc25df3d/L0KzQYm3VMI2N5RBiZH0aYP2gLBuTfdzaaoyj9H1Zj3vf7j2TgBpd6V0fARqcHj8Pcj2jPYudJDsh58AYXWzQYXpWcFka2RqSZCAMkC5RYm5VsE2OmU7S6YDMEm0QYiBTwBvbz==/kisspng-gray-wolf-logo-photography-wolf-logo-5ae014b91cc3e1.5206582615246348091178.png" width="30" height="30" alt="" />
+          {/* <img src="https://png2.cleanpng.com/sh/63c5cba8e6b11ffe4e9608fafc25df3d/L0KzQYm3VMI2N5RBiZH0aYP2gLBuTfdzaaoyj9H1Zj3vf7j2TgBpd6V0fARqcHj8Pcj2jPYudJDsh58AYXWzQYXpWcFka2RqSZCAMkC5RYm5VsE2OmU7S6YDMEm0QYiBTwBvbz==/kisspng-gray-wolf-logo-photography-wolf-logo-5ae014b91cc3e1.5206582615246348091178.png" width="30" height="30" alt="" /> */}
         </a>
         <a className="navbar-brand" href="/">wildMovies</a>
         <ul className="navbar-nav mr-auto">
